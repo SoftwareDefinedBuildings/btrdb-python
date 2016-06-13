ca0bd7623ffda3af5d1d19ac6edffac222bcfe94
Sam Kumar <samkumar99@gmail.com>
2016-06-12 21:56:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
   author = 'Sam Kumar, Michael P Andersen',
   author_email = 'samkumar99@gmail.com, michael@steelcode.com',
   url = 'https://github.com/SoftwareDefinedBuildings/btrdb-python',
-  download_url = 'https://github.com/SoftwareDefinedBuildings/tarball/1.0',
-  install_requires= ["pycapnp >= 0.5.8"],
+  download_url = 'https://github.com/SoftwareDefinedBuildings/btrdb-python/tarball/1.0',
+  install_requires = ["pycapnp >= 0.5.8"],
   keywords = ['btrdb', 'timeseries', 'database'],
   classifiers = []
 )
