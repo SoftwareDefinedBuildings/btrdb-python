f81e15bf4e8df12877bab5ab66a60a251e9f7dc2
Sam Kumar <samkumar99@gmail.com>
2016-08-19 10:40:29 -0700

    Change name so as to not conflict with the HTTP bindings

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 setup(
-  name = 'btrdb',
-  packages = ['btrdb'],
+  name = 'btrdbcapnp',
+  packages = ['btrdbcapnp'],
   version = '1.0',
-  description = 'Bindings to interact with the Berkeley Tree Database',
+  description = "Bindings to interact with the Berkeley Tree Database using the Cap'n Proto Interface",
   author = 'Sam Kumar, Michael P Andersen',
   author_email = 'samkumar99@gmail.com, michael@steelcode.com',
   url = 'https://github.com/SoftwareDefinedBuildings/btrdb-python',
   download_url = 'https://github.com/SoftwareDefinedBuildings/btrdb-python/tarball/1.0',
-  package_data = { 'btrdb': ['interface.capnp'] },
+  package_data = { 'btrdbcapnp': ['interface.capnp'] },
   include_package_data = True,
   install_requires = ["pycapnp >= 0.5.8"],
-  keywords = ['btrdb', 'timeseries', 'database'],
+  keywords = ['btrdb', 'timeseries', 'database', 'capn', 'capnp', "Cap'n Proto"],
   classifiers = []
 )
